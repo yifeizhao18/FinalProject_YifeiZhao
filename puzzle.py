68eba56d0704dedea4511cfb625136548489dde1
yifeizhao18 <122106660+yifeizhao18@users.noreply.github.com>
2023-06-07 15:22:47 +0000

    edited the game

--- puzzle.py
+++ puzzle.py
@@ -4,5 +4,5 @@ puzzle2 = "The day before two days after the day before tomorrow is Saturday. Wh
 puzzle3 = "1. M is taller than N, but poorer than R. \n2. N is taller than Q, but richer than R. \n3. O is shorter than P, but richer than M. \n4. P is shorter and poorer than Q. \n5. Q is shorter and poorer than M. \n6. R is taller than M and richer than O. \nWho is the tallest person out of the 6 people? "
 puzzle4 = "What is the alphabet after E? \nO, T, T, F, F, S, S, E, ? "
 puzzle5 = "M's mother has five daughters: R, T, S, A. Who is the fifth daughter? "
-puzzle6 = "Pointing towards S, M said, 'The father of her brother is the only son of my grandfather. ' How is S related to M? "
+puzzle6 = "Pointing towards Katy, Amanda said, 'The father of her brother is the only son of my grandfather. ' How is Katy related to Amanda? "
 puzzle7 = "What is the next alphabet? \nD, N, O, S, A, J, J, ? "
